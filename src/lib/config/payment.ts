87cc0510b8b5fa3dc3c736b8573b331446a156b3
rihid <rizalhidayat006@gmail.com>
2025-03-06 16:11:17 +0700

    change payment config to prod

--- src/lib/config/payment.ts
+++ src/lib/config/payment.ts
@@ -1,7 +1,7 @@
 import { MidtransClient } from "midtrans-node-client";
 
 export const snap = new MidtransClient.Snap({
-  isProduction: false,
+  isProduction: true,
   serverKey: process.env.MIDTRANS_SERVER_KEY || '',
   clientKey: process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || '',
 })
